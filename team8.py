952a9ff74dd0d376ea2b2c7513c651ce3b88c187
sarthak singhal <sarthak.singhal@students.iiit.ac.in>
2019-03-01 18:53:38 +0530

    v3

--- team8.py
+++ team8.py
@@ -17,14 +17,15 @@ class Player8:
         """
         Initialize variables
         """
-        self.default=(1,1,1)#default move
-        self.limit=23.5#time limit
+        self.default=(0,4,4)#default move
+        self.limit=6#time limit
         self.start=0#start time
+        # self.maxdepth=3
         self.maxdepth=9*9*9
         self.player=0#x=1 o=0
         self.opponent=0
         self.bestmv=(0,0,0)
-        self.inf = 100000000
+        self.inf = 10000000000
         self.max_player = 1
         self.map_symbol = ['o', 'x']
         self.zob_store = []
@@ -33,6 +34,8 @@ class Player8:
         self.last_blk_won = 0
         self.numsteps = 0
         self.dict = {}
+        self.blockweight=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+        # self.blockweight=[1,.9,1,.9,1.1,.9,1,.9,1,1,.9,1,.9,1.1,.9,1,.9,1]
         for i in range(36):
             self.zob_store.append(2**i)
 
@@ -40,21 +43,23 @@ class Player8:
 
     def smallboardutility(self,board,symbol):
         """
-        Find small board utility
+        Find weights of each cell
         """
 
         for b in range(2):
 
-            #r1 r2 r3 c1 c2 c3 d1 d2
-            utilityvector=[]
-            
             #check r1 r2 r3
             for i in range(3):
                 row=[]
                 row.append(board.small_boards_status[b][i][0])
                 row.append(board.small_boards_status[b][i][1])
                 row.append(board.small_boards_status[b][i][2])
-                utilityvector.append(self.calculatewincombsb(row,symbol))
+                
+                temp=self.calculatewincombsb(row,symbol)
+                
+                self.blockweight[b*9+i*3]+=temp
+                self.blockweight[b*9+i*3+1]+=temp
+                self.blockweight[b*9+i*3+2]+=temp
 
             #check c1 c2 c3
             for i in range(3):
@@ -62,33 +67,85 @@ class Player8:
                 col.append(board.small_boards_status[b][0][i])
                 col.append(board.small_boards_status[b][1][i])
                 col.append(board.small_boards_status[b][2][i])
-                utilityvector.append(self.calculatewincombsb(col,symbol))
+
+                temp=self.calculatewincombsb(col,symbol)
+
+                self.blockweight[b*9+i]+=temp
+                self.blockweight[b*9+i+3]+=temp
+                self.blockweight[b*9+i+6]+=temp
+
 
             #check dig1
             dig=[]
             dig.append(board.small_boards_status[b][0][0])
             dig.append(board.small_boards_status[b][1][1])
             dig.append(board.small_boards_status[b][2][2])
-            utilityvector.append(self.calculatewincombsb(dig,symbol))
+            
+            temp=self.calculatewincombsb(dig,symbol)
+
+            self.blockweight[b*9]+=temp
+            self.blockweight[b*9+4]+=temp
+            self.blockweight[b*9+8]+=temp
 
             #check dig2
             dig=[]
             dig.append(board.small_boards_status[b][0][2])
             dig.append(board.small_boards_status[b][1][1])
             dig.append(board.small_boards_status[b][2][0])
-            utilityvector.append(self.calculatewincombsb(dig,symbol))
 
-        #modify later
-        return(sum(utilityvector))
+            temp=self.calculatewincombsb(dig,symbol)
+            
+            self.blockweight[b*9+2]+=temp
+            self.blockweight[b*9+4]+=temp
+            self.blockweight[b*9+6]+=temp
 
 
 
-    def calculatewincomb(self,v,symbol):
+    def calculatewincombsb(self,v,symbol):
+        """
+        Calculate weight from each row,column,dig
+        """
+
+        weight=0
+
+        if symbol=='x':
+            opp='o'
+        else:
+            opp='x'
+
+        #checking edge case
+        if v.count(symbol)==2 and v.count('-')==1:
+            weight=100
+        elif v.count(symbol)==3:
+            weight=1000
+        elif v.count(opp)==2 and v.count('-')==1:
+            weight=-110
+        elif v.count(opp)==3:
+            weight=-1010
+
+        #checking normal cases
+        if 'd' in v:
+            weight=-0.5
+        elif ((symbol in v) and (opp in v)):
+            weight=-0.5 
+        elif (symbol in v) and (opp not in v):
+            weight+=5
+        elif (opp in v) and (symbol not in v):
+            weight+=-5
+        elif ('-' in v) and (symbol not in v) and (opp not in v):
+            weight+=0   
+           
+        return weight
+
+
+
+    def calculatewincomb(self,v,symbol,blkno):
         """
         Calculate status of each row,column,dig
         """
 
-        utility=0
+        blkwt=self.blockweight[blkno]
+        utility=200
 
         if symbol=='x':
             opp='o'
@@ -98,61 +155,47 @@ class Player8:
         #checking edge cases
         if v.count(symbol)==2 and v.count('-')==1:
             utility+=10
+            if blkwt<0:
+                blkwt+=-blkwt+10000
+            else:
+                blkwt+=10000
+
         elif v.count(symbol)==3:
             utility+=100
+            if blkwt<0:
+                blkwt+=-blkwt+100000
+            else:
+                blkwt+=100000
+
         elif v.count(opp)==2 and v.count('-')==1:
             utility-=10
+            if blkwt<0:
+                blkwt+=-10100
+            else:
+                blkwt+=-blkwt-10100
+
         elif v.count(opp)==3:
             utility-=100
+            if blkwt<0:
+                blkwt+=-110000
+            else:
+                blkwt+=-blkwt-110000
 
        #checking normal cases
         if (symbol in v) and (opp not in v):
             utility+=1
+            blkwt+=.5
         elif (opp in v) and (symbol not in v):
-            utility+=-1
+            utility+=-1.1
+            blkwt-=.5
         elif ('-' in v) and (symbol not in v) and (opp not in v):
             utility+=.5
         elif (symbol in v) and (opp in v):
-            utility+=0 
+            utility+=0
+            blkwt-=.3 
            
-        return utility
 
-
-
-    def calculatewincombsb(self,v,symbol):
-        """
-        Calculate status of each row,column,dig
-        """
-
-        utility=0
-
-        if symbol=='x':
-            opp='o'
-        else:
-            opp='x'
-
-        #checking edge case
-        if v.count(symbol)==2 and v.count('-')==1:
-            utility+=10
-        elif v.count(symbol)==3:
-            utility+=100
-        elif v.count(opp)==2 and v.count('-')==1:
-            utility-=10
-        elif v.count(opp)==3:
-            utility-=100
-
-        #checking normal cases
-        if 'd' in v:
-            utility+=0 
-        elif ((symbol in v) and (opp in v)):
-            utility+=0 
-        elif (symbol in v) and (opp not in v):
-            utility+=1
-        elif (opp in v) and (symbol not in v):
-            utility+=-1
-        elif ('-' in v) and (symbol not in v) and (opp not in v):
-            utility+=.5            
-           
+        self.blockweight[blkno]=blkwt
         return utility
 
 
@@ -164,14 +207,18 @@ class Player8:
 
         #r1 r2 r3 c1 c2 c3 d1 d2
         utilityvector=[]
-
+        blkno=9*b+(3*r+c)/3
+      
         #check r1 r2 r3
         for i in range(3):
             row=[]
             row.append(board.big_boards_status[b][r+i][c])
             row.append(board.big_boards_status[b][r+i][c+1])
             row.append(board.big_boards_status[b][r+i][c+2])
-            utilityvector.append(self.calculatewincomb(row,symbol))
+
+            temp=self.calculatewincomb(row,symbol,blkno)
+            utilityvector.append(temp)
+            
 
         #check c1 c2 c3
         for i in range(3):
@@ -179,21 +226,33 @@ class Player8:
             col.append(board.big_boards_status[b][r][c+i])
             col.append(board.big_boards_status[b][r+1][c+i])
             col.append(board.big_boards_status[b][r+2][c+i])
-            utilityvector.append(self.calculatewincomb(col,symbol))
+
+            temp=self.calculatewincomb(col,symbol,blkno)
+            utilityvector.append(temp)
+            
+
 
         #check dig1
         dig=[]
         dig.append(board.big_boards_status[b][r][c])
         dig.append(board.big_boards_status[b][r+1][c+1])
         dig.append(board.big_boards_status[b][r+2][c+2])
-        utilityvector.append(self.calculatewincomb(dig,symbol))
+
+        temp=self.calculatewincomb(dig,symbol,blkno)
+        utilityvector.append(temp)
+        
+
 
         #check dig2
         dig=[]
         dig.append(board.big_boards_status[b][r][c+2])
         dig.append(board.big_boards_status[b][r+1][c+1])
         dig.append(board.big_boards_status[b][r+2][c])
-        utilityvector.append(self.calculatewincomb(dig,symbol))
+
+        temp=self.calculatewincomb(dig,symbol,blkno)
+        utilityvector.append(temp)
+        
+
 
         return(sum(utilityvector))
 
@@ -204,23 +263,36 @@ class Player8:
         Heuristic function
         """
         utility=0
-        
-        #calculating small board utility and weights of each block
-        utility+=self.smallboardutility(board,symbol)
+        # self.blockweight=[1,.9,1,.9,1.1,.9,1,.9,1,1,.9,1,.9,1.1,.9,1,.9,1]
+        self.blockweight=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+        #calculating weights of each block
+        self.smallboardutility(board,symbol)
         
         #calculating big board utility 
         for i in range(2):
             for j in range(3):
                 for k in range(3):
-                    utility+=self.blockutility(board,i,3*j,k*3,symbol)
+                    a=self.blockutility(board,i,3*j,k*3,symbol)
+                    b=self.blockweight[9*i+j*3+k]
+                    utility+=a*b
 
-################TESTING##################################
+       
+
+        # if symbol!=self.map_symbol[self.player]:
+            # utility=-utility
+        
+
+########TESTING##################################
         # print "###############board vs utility####################33"
         # board.print_board()
         # print utility
         # print "###############board vs utility####################33"
+        # print
+        # print
+        # print
         # a=raw_input()
-######TESTING############################################
+#################TESTING############################################
 
         return(utility)
 
@@ -264,6 +336,16 @@ class Player8:
         """
         minimax+alphabeta
         """
+
+##########################testing#################################
+        # print "#############testing###################33"
+        # print "self_player"
+        # print self.player
+        # print "player"
+        # print prev
+        # a=raw_input()
+        # print "#############testing###################33"
+##########################testing#################################
 
         if time() - self.start > self.limit:
             return self.utility(board,self.map_symbol[self.player])
@@ -365,19 +447,31 @@ class Player8:
             
             self.update_hashtable(moves,player)
 
-##################################testing#################################
-            # print "############utility test###############33"
-            # print player_utility
-            # print curmax
-            # print "############utility test###############33"
-            # a=raw_input()
-##################################testing#################################
-            if(player_utility > curmax):
+
+            if player_utility > curmax and player==self.max_player:
                 cur_best_move = moves
                 curmax = player_utility
+           
+
+
+##########################testing#################################
+            # print "############curmax test###############33"
+            # print player_utility
+            # print "curmax is below"
+            # print curmax
+            # print "############curmax test###############33"
+            # a=raw_input()
+##########################testing#################################
         
         self.bonus_move_cur[player] = tempbonusmove
         
+##########################testing#################################
+        # print "##########final utility##################"
+        # print curmax
+        # a=raw_input()
+        # print "##########final utility##################"
+##########################testing#################################
+
         return cur_best_move
    
 
@@ -390,12 +484,12 @@ class Player8:
 
 			# self.transpositionTable={}
 
-###########################testing###########################
+#####################testing###########################
             # print "######depth test#########"
             # print depth
             # print "######depth test#########"
             # a=raw_input()
-###########################testing###########################
+#####################testing###########################
 
             if(time()-self.start)>self.limit:
                 break
@@ -412,9 +506,11 @@ class Player8:
 
         #initialising players
         if symbol=='x':
+            self.max_player=1
             self.player=1
             self.opponent=0
         else:
+            self.max_player=0
             self.player=0
             self.opponent=1
 
@@ -430,6 +526,12 @@ class Player8:
             depth=1
 
             tempboard=deepcopy(gameboard)
+            self.bonus_move_cur=[0,0]
+            if(self.last_blk_won):
+                self.bonus_move_cur[self.max_player]^=1
+            else:
+                self.bonus_move_cur[self.max_player]=0
+
             tempmove=self.idfs(tempboard,oldmove,depth,self.player)
 
             status,blk_won=tempboard.update(oldmove,tempmove,self.map_symbol[self.player])
